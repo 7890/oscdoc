5b43f13a2fd32413c63f3db6371ec43567e4e0d1
Thomas Brand <tom@trellis.ch>
2014-07-18 19:56:42 +0200

    echo to stderr

--- src/oscdoc.sh
+++ src/oscdoc.sh
@@ -10,7 +10,7 @@ RES_DIR=$DIR/oscdoc_res
 
 if [ $# -ne 2 ]
 then
-	echo "need params: <oscdoc xml file> <output directory>"
+	echo "need params: <oscdoc xml file> <output directory>" >&2
 	exit
 fi
 
@@ -19,9 +19,9 @@ OUTPUT_DIR="$2"
 
 print_label()
 {
-	echo ".------"
-	echo "| $1"
-	echo "\______"
+	echo ".------" >&2
+	echo "| $1" >&2
+	echo "\______" >&2
 }
 
 checkAvail()
@@ -43,53 +43,53 @@ for tool in {xmlstarlet,sed,diff,bc,oschema_validate}; \
 if [ ! -e "$DEFINITION" ]
 then
         echo "xml file not found!" >&2
-        echo "$DEFINITION"
+        echo "$DEFINITION" >&2
         exit 1
 fi
 
 if [ ! -e "$OUTPUT_DIR" ]
 then
         echo "output directory does not exist!" >&2
-        echo "$OUTPUT_DIR"
+        echo "$OUTPUT_DIR" >&2
         exit 1
 fi
 
 if [ ! -e "$XSL1" ]
 then
         echo "stylesheet not found!" >&2
-        echo "$XSL1"
+        echo "$XSL1" >&2
         exit 1
 fi
 
 if [ ! -e "$XSL2" ]
 then
         echo "stylesheet not found!" >&2
-        echo "$XSL2"
+        echo "$XSL2" >&2
         exit 1
 fi
 
 if [ ! -e "$RES_DIR" ]
 then
         echo "ressources dir not found!" >&2
-        echo "$RES_DIR"
+        echo "$RES_DIR" >&2
         exit 1
 fi
 
-echo -n "checking if xml file is valid... "
+echo -n "checking if XML file is valid... " >&2
 
 a=`"$VAL_SCRIPT" "$DEFINITION" 2>&1`
 ret=$?
 if [ "x$ret" != "x0" ]
 then
-	echo "NO"
-	echo "reason is given below:"
-	echo "$a"
+	echo "NO ($DEFINITION)" >&2
+	echo "reason is given below:" >&2
+	echo "$a" >&2
 	exit 1
 else
-	echo "yes"
+	echo "yes ($DEFINITION)" >&2
 fi
 
-echo "creating xml_data.js..."
+echo "creating xml_data.js..." >&2
 
 cat "$DEFINITION" | sed "s/'/\\\'/g" > "$OUTPUT_DIR"/tmp.def
 
@@ -99,7 +99,7 @@ cat "$DEFINITION" | sed "s/'/\\\'/g" > "$OUTPUT_DIR"/tmp.def
 
 rm -f "$OUTPUT_DIR"/tmp.def
 
-echo "creating divs.out..."
+echo "creating divs.out..." >&2
 
 xmlstarlet tr "$XSL1" \
 "$DEFINITION" \
@@ -113,23 +113,23 @@ xmlstarlet tr "$XSL1" \
 	| sed 's/_INF_/\&infin;/g' \
 	> "$OUTPUT_DIR"/divs.out
 
-echo "creating index.out..."
+echo "creating index.out..." >&2
 
 xmlstarlet tr "$XSL2" \
 "$DEFINITION" \
 > "$OUTPUT_DIR"/index.out
 
-echo "creating index.html..."
+echo "creating index.html..." >&2
 
 sed "/<!--DIVS-->/r "$OUTPUT_DIR"/divs.out" "$OUTPUT_DIR"/index.out > "$OUTPUT_DIR"/index.html
 #| sed '/<!--DIVS-->/r div_.out' -
 
-echo "cleaning up..."
+echo "cleaning up..." >&2
 
 rm "$OUTPUT_DIR"/divs.out
 rm "$OUTPUT_DIR"/index.out
 
-echo "copying ressources to $OUTPUT_DIR..."
+echo "copying ressources to $OUTPUT_DIR..." >&2
 
 #copy needed resources
 mkdir -p "$OUTPUT_DIR"/res
@@ -138,6 +138,6 @@ mv "$OUTPUT_DIR"/xml_data.js "$OUTPUT_DIR"/res
 
 cp "$RES_DIR"/* "$OUTPUT_DIR"/res
 
-echo "output:"
-echo "$OUTPUT_DIR/index.html"
-echo "done oscdoc"
+echo "output:" >&2
+echo "$OUTPUT_DIR/index.html" >&2
+echo "done oscdoc" >&2
