6b1cbba327fc25858cec4f6152d7f15fad88dbe8
Thomas Brand <tom@trellis.ch>
2014-07-21 10:15:02 +0200

    add aspect_map (experimental)

--- src/oscdoc.sh
+++ src/oscdoc.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#oscdoc is part of https://github.com/7890/oscdoc
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -11,7 +12,7 @@ RES_DIR=$DIR/oscdoc_res
 if [ $# -ne 2 ]
 then
 	echo "need params: <oscdoc xml file> <output directory>" >&2
-	exit
+	exit 1
 fi
 
 DEFINITION="$1"
@@ -36,50 +37,50 @@ checkAvail()
 	fi
 }
 
-for tool in {xmlstarlet,sed,diff,bc,oschema_validate}; \
+for tool in {xmlstarlet,sed,diff,bc,oschema_validate,oscdoc_aspect_map,dot,convert,oscdoc_aspect_graph}; \
 	do checkAvail "$tool"; done
 
 
 if [ ! -e "$DEFINITION" ]
 then
-        echo "xml file not found!" >&2
-        echo "$DEFINITION" >&2
-        exit 1
+	echo "xml file not found!" >&2
+	echo "$DEFINITION" >&2
+	exit 1
 fi
 
 if [ ! -e "$OUTPUT_DIR" ]
 then
-        echo "output directory does not exist!" >&2
-        echo "$OUTPUT_DIR" >&2
-        exit 1
+	echo "output directory does not exist!" >&2
+	echo "$OUTPUT_DIR" >&2
+	exit 1
 fi
 
 if [ ! -e "$XSL1" ]
 then
-        echo "stylesheet not found!" >&2
-        echo "$XSL1" >&2
-        exit 1
+	echo "stylesheet not found!" >&2
+	echo "$XSL1" >&2
+	exit 1
 fi
 
 if [ ! -e "$XSL2" ]
 then
-        echo "stylesheet not found!" >&2
-        echo "$XSL2" >&2
-        exit 1
+	echo "stylesheet not found!" >&2
+	echo "$XSL2" >&2
+	exit 1
 fi
 
 if [ ! -e "$RES_DIR" ]
 then
-        echo "ressources dir not found!" >&2
-        echo "$RES_DIR" >&2
-        exit 1
+	echo "ressources dir not found!" >&2
+	echo "$RES_DIR" >&2
+	exit 1
 fi
 
 echo -n "checking if XML file is valid... " >&2
 
 a=`"$VAL_SCRIPT" "$DEFINITION" 2>&1`
 ret=$?
-if [ "x$ret" != "x0" ]
+if [ $ret -ne 0 ]
 then
 	echo "NO ($DEFINITION)" >&2
 	echo "reason is given below:" >&2
@@ -88,6 +89,47 @@ then
 else
 	echo "yes ($DEFINITION)" >&2
 fi
+
+#assemble referenced asspects to generate a graph showing the
+#"mount points"
+
+#this is experimental and redundant and slow
+
+#the meta/doc_origin must point to an existing file
+#starting with http for remote or file://xxx (the latter only works
+#locally for testing reasons)
+
+aspect_map="`mktemp`"
+oscdoc_aspect_map "$DEFINITION" > "$aspect_map"
+ret=$?
+if [ $ret -ne 0 ]
+then
+	echo "could not create aspect map!" >&2
+	rm -f "$aspect_map"
+	exit 1
+fi
+
+graph_svg="`mktemp`".svg
+graph_tl_png="`mktemp`".png
+
+oscdoc_aspect_graph "$aspect_map" "$graph_svg" "$graph_tl_png"
+ret=$?
+if [ $ret -ne 0 ]
+then
+	echo "could not create aspects graph!" >&2
+	rm -f "$aspect_map"
+	rm -f "$graph_svg"
+	rm -f "$graph_tl_png"
+	exit 1
+fi
+
+##
+#prepare copy of needed resources for html page
+mkdir -p "$OUTPUT_DIR"/res
+
+mv "$aspect_map" "$OUTPUT_DIR"/res/aspect_map.xml
+mv "$graph_svg" "$OUTPUT_DIR"/res/aspects_graph.svg
+mv "$graph_tl_png" "$OUTPUT_DIR"/res/aspects_graph_tl.png
 
 echo "creating xml_data.js..." >&2
 
@@ -115,7 +157,10 @@ xmlstarlet tr "$XSL1" \
 
 echo "creating index.out..." >&2
 
+#params relative to index.html
 xmlstarlet tr "$XSL2" \
+-s aspects_graph_tl="res/aspects_graph_tl.png" \
+-s aspects_graph_svg="res/aspects_graph.svg" \
 "$DEFINITION" \
 > "$OUTPUT_DIR"/index.out
 
@@ -131,12 +176,12 @@ rm "$OUTPUT_DIR"/index.out
 
 echo "copying ressources to $OUTPUT_DIR..." >&2
 
-#copy needed resources
-mkdir -p "$OUTPUT_DIR"/res
+#copy other needed resources for html page and archive
 
 mv "$OUTPUT_DIR"/xml_data.js "$OUTPUT_DIR"/res
 
 cp "$RES_DIR"/* "$OUTPUT_DIR"/res
+cp "$DEFINITION" "$OUTPUT_DIR"/res/unit.orig.xml
 
 echo "output:" >&2
 echo "$OUTPUT_DIR/index.html" >&2
